85e539cbe9b53b6aea8479dcf4728f1225b5fdfa
John <john@real-currents.com>
2022-04-08 10:51:53 -0400

    add global survival intro

--- try.js
+++ try.js
@@ -14,17 +14,17 @@ async function main () {
 
     console.log('Version:', version.version);
 
-    // startipfs daemon API server
+    // start the = API server
     const ipfsAPI = new HttpApi(node) // leaving out the arguments will default to these values
     await ipfsAPI.start();
 
-    console.log("API Server", await ipfsAPI.apiAddr);
+    // console.log("API Server", await ipfsAPI.apiAddr);
 
     // // start the API gateway
     const gateway = new HttpGateway(node);
     await gateway.start();
 
-    console.log("Local Gateway ", await gateway._ipfs.config.getAll());
+    // console.log("Local Gateway ", await gateway._ipfs.config.getAll());
     // // ...
 
     // CREATE A DIR
@@ -132,19 +132,81 @@ async function main () {
     } finally {}
 
 
+    // try {
+    //
+    //     const single_page_content = [];
+    //     const read_content_promise = new Promise((resolve, reject) => {
+    //         fs.createReadStream('./' + dir_path+ '/index.html')
+    //             .pipe(es.split())
+    //             .pipe(
+    //                 es.mapSync((line) => {
+    //                     // console.log(line);
+    //                     single_page_content.push(line);
+    //                 })
+    //                     .on('error', (err) => console.log(err))
+    //                     .on('end', () => resolve(single_page_content))
+    //             )
+    //     });
+    //
+    //     read_content_promise.then(async (content) => {
+    //         console.log(content.join("\n"))
+    //
+    //         // add your data to to IPFS - this can be a string, a Buffer,
+    //         // a stream of Buffers, etc
+    //         //const single_page_metadata = await node.add(single_page_content);
+    //         const single_page_metadata = await node
+    //             // .addAll(
+    //             //     globSource('.', dir_path, { recursive: true })
+    //             // );
+    //             .addAll([{
+    //                 path: '/' + dir_path + '/index.html',
+    //                 content: content.join("\n")
+    //             }]);
+    //
+    //         console.log(single_page_metadata);
+    //
+    //         const content_paths = [];
+    //
+    //         for await (const metadata of single_page_metadata) {
+    //             console.log(metadata);
+    //
+    //             if ('path' in metadata) {
+    //                 if (metadata['path'].match(dir_path) !== null && metadata['path'].replace(dir_path, "") === "") {
+    //                     dir_cid_path = metadata['cid'].toString();
+    //                     content_paths.forEach((path, i, a) => {
+    //                         a[i] = path.replace(dir_path, dir_cid_path);
+    //                     })
+    //                     content_paths.push(`https://ipfs.io/ipfs/${dir_cid_path}`);
+    //
+    //                 } else {
+    //                     content_paths.push(`https://ipfs.io/ipfs/${metadata['path']}`);
+    //                 }
+    //             }
+    //         }
+    //
+    //         content_paths.forEach((path) => {
+    //             console.log(`${path}`);
+    //         })
+    //     });
+    // } finally {}
+
+
     try {
 
-        const single_page_content = [];
+        const quest_path = "quest";
+        dir_cid_path = quest_path;
+
+        const quest_log_content = [];
         const read_content_promise = new Promise((resolve, reject) => {
-            fs.createReadStream('./' + dir_path+ '/index.html')
+            fs.createReadStream('./' + quest_path + '/index.html')
                 .pipe(es.split())
                 .pipe(
                     es.mapSync((line) => {
                         // console.log(line);
-                        single_page_content.push(line);
+                        quest_log_content.push(line);
                     })
                         .on('error', (err) => console.log(err))
-                        .on('end', () => resolve(single_page_content))
+                        .on('end', () => resolve(quest_log_content))
                 )
         });
 
@@ -153,28 +215,89 @@ async function main () {
 
             // add your data to to IPFS - this can be a string, a Buffer,
             // a stream of Buffers, etc
-            //const single_page_metadata = await node.add(single_page_content);
-            const single_page_metadata = await node
+            //const quest_log_metadata = await node.add(quest_log_content);
+            const quest_log_metadata = await node
                 // .addAll(
-                //     globSource('.', dir_path, { recursive: true })
+                //     globSource('.', quest_path, { recursive: true })
                 // );
                 .addAll([{
-                    path: '/' + dir_path + '/index.html',
+                    path: '/' + quest_path + '/index.html',
                     content: content.join("\n")
                 }]);
 
-            console.log(single_page_metadata);
+            console.log(quest_log_metadata);
 
             const content_paths = [];
 
-            for await (const metadata of single_page_metadata) {
+            for await (const metadata of quest_log_metadata) {
                 console.log(metadata);
 
                 if ('path' in metadata) {
-                    if (metadata['path'].match(dir_path) !== null && metadata['path'].replace(dir_path, "") === "") {
+                    if (metadata['path'].match(quest_path) !== null && metadata['path'].replace(quest_path, "") === "") {
                         dir_cid_path = metadata['cid'].toString();
                         content_paths.forEach((path, i, a) => {
-                            a[i] = path.replace(dir_path, dir_cid_path);
+                            a[i] = path.replace(quest_path, dir_cid_path);
+                        })
+                        content_paths.push(`https://ipfs.io/ipfs/${dir_cid_path}`);
+
+                    } else {
+                        content_paths.push(`https://ipfs.io/ipfs/${metadata['path']}`);
+                    }
+                }
+            }
+
+            content_paths.forEach((path) => {
+                console.log(`${path}`);
+            })
+        });
+    } finally {}
+
+    try {
+
+        const global_survival_path = "global-survival";
+        dir_cid_path = global_survival_path;
+
+        const global_survival_log_content = [];
+        const read_content_promise = new Promise((resolve, reject) => {
+            fs.createReadStream('./' + global_survival_path + '/index.html')
+                .pipe(es.split())
+                .pipe(
+                    es.mapSync((line) => {
+                        // console.log(line);
+                        global_survival_log_content.push(line);
+                    })
+                        .on('error', (err) => console.log(err))
+                        .on('end', () => resolve(global_survival_log_content))
+                )
+        });
+
+        read_content_promise.then(async (content) => {
+            console.log(content.join("\n"))
+
+            // add your data to to IPFS - this can be a string, a Buffer,
+            // a stream of Buffers, etc
+            //const global_survival_log_metadata = await node.add(global_survival_log_content);
+            const global_survival_log_metadata = await node
+                // .addAll(
+                //     globSource('.', global_survival_path, { recursive: true })
+                // );
+                .addAll([{
+                    path: '/' + global_survival_path + '/index.html',
+                    content: content.join("\n")
+                }]);
+
+            console.log(global_survival_log_metadata);
+
+            const content_paths = [];
+
+            for await (const metadata of global_survival_log_metadata) {
+                console.log(metadata);
+
+                if ('path' in metadata) {
+                    if (metadata['path'].match(global_survival_path) !== null && metadata['path'].replace(global_survival_path, "") === "") {
+                        dir_cid_path = metadata['cid'].toString();
+                        content_paths.forEach((path, i, a) => {
+                            a[i] = path.replace(global_survival_path, dir_cid_path);
                         })
                         content_paths.push(`https://ipfs.io/ipfs/${dir_cid_path}`);
 
